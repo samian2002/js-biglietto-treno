a0c4ac3dc6e5cab990607db08bbd6d18bdcaa7da
samian2002 <samiantariq14@gmail.com>
2025-02-13 03:00:16 +0100

    implemento if

--- main.js
+++ main.js
@@ -6,3 +6,10 @@ const scontoAnziani = 0.4;
 let kmUtente = Number(prompt("Quanti km devi percorrere?"));
 let etaUtente = Number(prompt("Quanti anni hai?"));
 let costoBiglietto = prezzoPerKm * kmUtente;
+//utilizzo if per calcolare lo sconto
+if (etaUtente < 18) {  // sconto per minorenni
+    costoBiglietto -= costoBiglietto * scontoMinorenni; // costoBiglietto = costoBiglietto - (costoBiglietto * scontoMinorenni)
+} else if (etaUtente >= 65) {  // sconto per over 65
+    costoBiglietto -= costoBiglietto * scontoAnziani; // costoBiglietto = costoBiglietto - (costoBiglietto * scontoAnziani)
+}
+// stampo il risultato
