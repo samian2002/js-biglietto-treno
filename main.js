07adac4d9fd92ac1793e5c9966ce70681569cb31
samian2002 <samiantariq14@gmail.com>
2025-02-13 02:52:24 +0100

    dichiaro variabili

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-// dichiaro le i valori fissi
+// dichiaro i  valori fissi
 const prezzoPerKm = 0.21;
 const scontoMinorenni = 0.2;
 const scontoAnziani = 0.4;
+// dichiaro le variabili
+let kmUtente = Number(prompt("Quanti km devi percorrere?"));
+let etaUtente = Number(prompt("Quanti anni hai?"));
+let costoBiglietto = prezzoPerKm * kmUtente;
